26a4ebc28de549a0fcbb366caeffa08f21b359b3
Francis Diaz <diazh.francis@gmail.com>
2023-01-30 19:38:42 -0300

    NewCommit

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,32 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Navbar from './componentes/Navbar';
+import Cart from './componentes/Cart';
 import ItemListContainer from './componentes/ItemListContainer';
 import ItemDetailContainer from './componentes/ItemDetailContainer';
 import Error404 from './componentes/Error404';
+import CartContextProvider from './componentes/context/CartContext';
 
 
 
 function App() {
   return (
+    <CartContextProvider>
    <BrowserRouter>
     <Navbar/>
     <br/>
+    <h1>Bienvenidos a Dexter Fit</h1>
+    <h3>Nuestras recomendaciones:</h3>
     <Routes>
       <Route path={"/"} element={<ItemListContainer />}/>
       <Route path={"/category/:id"} element={<ItemListContainer />}/>
       <Route path={"/item/:id"} element={<ItemDetailContainer />}/>
+      <Route path={"/cart"} element={<Cart/>}/>
       <Route path={"*"} element={<Error404/>}/>
 
     </Routes>    
    </BrowserRouter>
+   </CartContextProvider>
   );
 }
 
